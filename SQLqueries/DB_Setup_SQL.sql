f7405b09d2b347a88bd33305e684d9503ea0074d
Kim Gustafsson <kigu25@student.bth.se>
2026-03-18 17:32:09 +0100

    Re-made the DB design to better fit endGoal

--- SQLqueries/DB_Setup_SQL.sql
+++ SQLqueries/DB_Setup_SQL.sql
@@ -3,32 +3,32 @@ USE drop_alert;
 
 CREATE TABLE store(
 	storeID INT PRIMARY KEY AUTO_INCREMENT,
-    storeName VARCHAR(60)
+    storeName VARCHAR(60) NOT NULL
 );
 
 CREATE TABLE productType(
 	typeID INT PRIMARY KEY AUTO_INCREMENT,
-    typeName VARCHAR(80)
+    typeName VARCHAR(80) NOT NULL
 );
 
 
 CREATE TABLE product(
-	productID int PRIMARY KEY AUTO_INCREMENT,
-    externalID int NOT NULL,
-    name VARCHAR(80),
-    typeID int NOT NULL,
+	productID INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(80) NOT NULL,
+    typeID INT NOT NULL,
     
-    FOREIGN KEY(typeID) REFERENCES ProductType(typeID)
+    FOREIGN KEY (typeID) REFERENCES productType(typeID)
 );
 
 
 CREATE TABLE inventory(
 	storeID INT NOT NULL,
-	productID INT NOT NULL,
+    productID INT NOT NULL,
+    externalID INT NOT NULL,
     price DECIMAL(10,2),
     quantity INT,
     
-    PRIMARY KEY(storeID, productID),
-    FOREIGN KEY(storeID) REFERENCES store(storeID),
-    FOREIGN KEY(productID) REFERENCES product(productID)
+    PRIMARY KEY (storeID, productID),
+    FOREIGN KEY (storeID) REFERENCES store(storeID),
+    FOREIGN KEY (productID) REFERENCES product(productID)
 );
