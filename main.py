19b3b8fba811af8ea48f16b37a95d9521c86240c
Kim Gustafsson <kigu25@student.bth.se>
2026-03-19 15:03:42 +0100

    Added a simple test webhook to discord

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from DB_Config.Db_init import get_connection
 from pre_scrapers.webhallen import get_productTypes, findItemsFromApi, matchItems, insert_matches
+from webbhooks.discord import discord_webhook
 
 def main():
 
@@ -18,6 +19,8 @@ def main():
     products, store = findItemsFromApi()
     matching_items = matchItems(productTypes, products)
     insert_matches(matching_items, store)
+    discord_webhook()
+
 
 
 if __name__ == "__main__":
