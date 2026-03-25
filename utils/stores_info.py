6e4cb8555b7823039b7c347cdf511c3422d4a288
Kim Gustafsson <kigu25@student.bth.se>
2026-03-25 21:36:03 +0100

    Added manatorsk URL

--- utils/stores_info.py
+++ utils/stores_info.py
@@ -1,7 +1,7 @@
 LINK_DICT = {
         
     "Webhallen" : "https://www.webhallen.com/api/productdiscovery/search/pokemon?page=1&touchpoint=DESKTOP&totalProductCountSet=false&origin=ORGANIC&sortBy=latest&limit=100",
-        
+    "Manatorsk" : "https://manatorsk-elastic-filter-production.up.railway.app/api/search"
     }
 
 
