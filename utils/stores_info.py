4d53a68f1b7801f2fc7b5343730670778c2f9fe4
Kim Gustafsson <kigu25@student.bth.se>
2026-03-26 20:49:12 +0100

    Prepared the stores_info to be able to buildl links for the program

--- utils/stores_info.py
+++ utils/stores_info.py
@@ -1,11 +1,26 @@
+# This variabel should also store how we build links 
 LINK_DICT = {
         
     "Webhallen" : "https://www.webhallen.com/api/productdiscovery/search/pokemon?page=1&touchpoint=DESKTOP&totalProductCountSet=false&origin=ORGANIC&sortBy=latest&limit=100",
     "Manatorsk" : "https://manatorsk-elastic-filter-production.up.railway.app/api/search"
+
     }
+
+
 
 
 
 
 def find_url(store):
     return LINK_DICT.get(store)
+
+
+
+def build_product_url(store, product_id):
+    pass
+
+
+
+
+def build_image_url():
+    pass
