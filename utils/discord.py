9d56703f3b157b557a3f21e6b0125f728da0afd9
Kim Gustafsson <kigu25@student.bth.se>
2026-03-26 16:18:12 +0100

    Added a new webhook to alert ADMIN of new products added to DB

--- utils/discord.py
+++ utils/discord.py
@@ -2,9 +2,9 @@ import os
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
 
-
+#TODO: This product_url build will prob not work for all sites NEED TO BE CHECKED
 def restock_webhook(item_name, price, store, itemID):
-    product_url = f"https://www.{store}.com/se/product/{itemID}"
+    product_url = f"https://www.{store}.com/product/{itemID}"
     webhook_url = os.getenv("DISCORD_WEBHOOK_URL")
 
     webhook = DiscordWebhook(url=webhook_url, username="Restock Alert")
@@ -29,6 +29,7 @@ def restock_webhook(item_name, price, store, itemID):
     webhook.execute()
 
 
+
 def pinger_webhook():
     webhook_url = os.getenv("DISCORD_PINGER_URL")
 
@@ -39,6 +40,29 @@ def pinger_webhook():
         description="VPS is still running - OK",
         color="008000"
     )
+
+    embed.set_timestamp()
+
+    webhook.add_embed(embed)
+    webhook.execute()
+
+
+
+
+def new_product_webhook(store, item_name, price):
+    webhook_url = os.getenv("DISCORD_NEW_PRODUCT_URL")
+
+    webhook = DiscordWebhook(url=webhook_url, username="ProductSniffer")
+
+    embed = DiscordEmbed(
+        title="New product detected",
+        description=f"New product at {store} detected and added to DB",
+        color="E74C3C"
+    )
+
+    embed.add_embed_field(name="Store", value=store, inline=True)
+    embed.add_embed_field(name="Name", value=item_name, inline=True)
+    embed.add_embed_field(name="Price", value=f"{price} kr", inline=True)
 
     embed.set_timestamp()
 
