b706a17b12399358176acfa5552387a24e66b975
Kim Gustafsson <kigu25@student.bth.se>
2026-03-28 17:22:04 +0100

    Fixed an edgecase where a empty stock value would crash the bot

--- utils/match_products.py
+++ utils/match_products.py
@@ -2,8 +2,15 @@ def match_items_webhallen(product_types, products):
 
     matching_items = []
 
+    sorted_product_types = sorted(product_types.items(), key=lambda x: len(x[0]), reverse=True)
+
+
     for key in products:
-       for type_name, type_id in product_types.items():
+        if not isinstance(key.get("stock"), dict):
+            print(f"SKIPPED (invalid stock) ID: {key['id']} Name: {key['name']}")
+            continue
+        
+        for type_name, type_id in sorted_product_types:
            if type_name in key["name"]:
                matching_items.append({
                    "name": key["name"],
@@ -12,6 +19,7 @@ def match_items_webhallen(product_types, products):
                    "price": float(key["price"]["price"]),
                    "quantity": key["stock"]["web"]
                })
+               break
     return matching_items
 
 
@@ -19,8 +27,10 @@ def match_items_webhallen(product_types, products):
 def match_items_manatorsk(product_types, products):
     matching_items = []
 
+    sorted_product_types = sorted(product_types.items(), key=lambda x: len(x[0]), reverse=True)
+
     for key in products:
-        for type_name, type_id in product_types.items():
+        for type_name, type_id in sorted_product_types:
             if type_name in key["title"]:
 
                 stripped_id = int(key["id"].split("/")[-1])
@@ -33,4 +43,5 @@ def match_items_manatorsk(product_types, products):
                     "handle": key["handle"],
                     "img_url": key["imgUrl"],
                 })
+                break
     return matching_items
